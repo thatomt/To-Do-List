6052cd058dd5bcffdb202dcc5d69cc06ac9615ab
Thato M <thatomsekele@gmail.com>
2026-02-21 11:52:13 +0200

    fixed git email

--- to_do_list.py
+++ to_do_list.py
@@ -1,7 +1,13 @@
 def show_tasks(tasks):
+    """
+    A function to display the current list of tasks in a to-do list application. 
+    It takes a list of tasks as input and prints them in a numbered format. 
+    If there are no tasks, it informs the user that there are no tasks yet.
+
+    """
+
     if not tasks:
         print("No tasks yet.")
     else:
         for i, task in enumerate(tasks, 1):
             print(f"{i}. {task}")
-
